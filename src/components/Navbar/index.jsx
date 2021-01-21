c7957bec7304fb683b41fb502f96e730fc2a2a38
Allana Caldas <allanacaldas@gmail.com>
2021-01-21 19:43:55 -0300

    Criando a Navbar e inserindo-a na Home

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "./poke-logo.png"
+
+const Navbar = (props) => {
+    return (
+        <div className="navbar">
+            <div className="navbar-logo">
+                <img className="logo-img" src={logo} alt="website logo"></img>
+            </div>
+            <div className="navbar-items">
+                <ul className="navbar-items-list">
+                    <li className="item"><a href="/">Todos PKMN</a></li>
+                    <li className="item"><a href="/">Meus PKMN</a></li>
+                    <li className="item"></li>
+                </ul>
+                <form className="form-search" action="action_page.php">
+                    <input type="text" placeholder="Search.." name="search"></input>
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div >
+    )
+}
+
+export default Navbar;
