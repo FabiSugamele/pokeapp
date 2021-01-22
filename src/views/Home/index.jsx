01a3f4c76006f220f821c0eca5c3f9b1811ef753
thejanesol <janainapereiratelecom@gmail.com>
2021-01-21 21:27:26 -0300

    botoes da paginacao

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -6,17 +6,35 @@ import "./Home.css";
 
 const Home = () => {
     const [pokemons, setPokemons] = useState([]);
-    const getPokemons =  () => {
-        axios.get("https://pokeapi.co/api/v2/pokemon/")
-        .then(res => setPokemons(res.data.results));
+    const [offset, setOffset] = useState(0);
+    const [currentPokemons, setCurrentPokemons] = useState(`https://pokeapi.co/api/v2/pokemon/?${offset}=0&limit=20`);
+
+    const getPokemons = () => {
+        axios.get(currentPokemons)
+            .then(res => setPokemons(res.data.results))
     };
 
     useEffect(() => {
         getPokemons();
     }, []);
 
+    function changePokemonsList() {
+        setCurrentPokemons(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`);
+        getPokemons(currentPokemons);
+        console.log(currentPokemons)
+    }
+
+    function next() {
+        changePokemonsList();
+        setOffset(offset + 20)
+    }
+
+    function previous() {
+        changePokemonsList();
+        offset >= 20 ? setOffset(offset - 20) : setOffset(0);
+    }
+
     const pokemonsList = pokemons.map((pokemon) => {
-        console.log(pokemon.name)
         return <PokemonCard name={pokemon.name} />
     })
 
@@ -26,6 +44,10 @@ const Home = () => {
             <section className="cards-conteiner">
                 {pokemonsList}
             </section>
+            <div className="button-pages">
+            <button className="page-btn" onClick={e => { previous() }}>Anterior</button>
+            <button className="page-btn" onClick={e => { next() }}>Próximo</button>
+            </div>
         </main>
     )
 
